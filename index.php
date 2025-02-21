9bc9f27606705ca89a8825d173fc1b3309a5c01d
Dmytro Kasko <dimoks77@gmail.com>
2025-02-21 19:30:32 +0100

    images

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/*   
+Theme Name: pvcstitch
+Theme URI: https://pvcstitch.com
+Description: A theme for pvcstitch.com
+Author: Dmytro Kasko   
+Author URI: https://dimoks.com  
+Version: 1.0.0
+*/
